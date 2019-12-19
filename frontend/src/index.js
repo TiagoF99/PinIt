c273acfecd263d49ca4d6b566b9c57ba7720a982
t.ferreria@live.ca <t.ferreira@live.ca>
2019-12-19 00:13:00 -0500

    added some front end stylings to the application

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/ie11';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
