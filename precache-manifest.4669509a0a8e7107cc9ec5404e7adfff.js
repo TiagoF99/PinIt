b9d857d03f767b75b03d5767a45a136402889e06
t.ferreria@live.ca <t.ferreira@live.ca>
2019-12-21 00:19:02 -0500

    Updates

---
+++ precache-manifest.4669509a0a8e7107cc9ec5404e7adfff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03383f33160cfde16900cacdaa2e18c9",
+    "url": "/idea/index.html"
+  },
+  {
+    "revision": "06c56d598225ac0dd1a0",
+    "url": "/idea/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c75691ecfe84d0daf445",
+    "url": "/idea/static/css/main.d2a1f6b0.chunk.css"
+  },
+  {
+    "revision": "06c56d598225ac0dd1a0",
+    "url": "/idea/static/js/2.25b5890a.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/idea/static/js/2.25b5890a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c75691ecfe84d0daf445",
+    "url": "/idea/static/js/main.a36a48cf.chunk.js"
+  },
+  {
+    "revision": "c8dcc5b585f7df529965",
+    "url": "/idea/static/js/runtime-main.ff35478e.js"
+  }
+]);
